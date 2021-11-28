70044653cd178663aea5f045f825083e684b2587
Mykola-Veres <veresnickolay@gmail.com>
2021-11-28 11:16:15 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import "./js/main";
 import "./js/tabs";
-// import "./js/chartCSV";
+import "./js/asyncAwait";
 import "./js/fetchQuack"
 
--- src/js/chartCSV.js
+++
@@ -1,2 +0,0 @@
-import { fetchRequestQuack } from "./fetchQuack";
-
